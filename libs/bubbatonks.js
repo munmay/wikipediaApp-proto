62f68f2514fab7082cc210d9673673faf65102a2
May <munmay.g@gmail.com>
2014-04-21 12:48:21 -0700

    Restart

---
+++ libs/bubbatonks.js
@@ -0,0 +1,26 @@
+(function (e, t) { 
+	e.fn.bubbaTonks = function (e, t, n, r) { 
+		if (typeof n == "undefined") { 
+			n = false } var i = null; if (typeof r != "undefined") { 
+				i = this.find(r).first() 
+			} 
+		else { i = this.children().first() } 
+		if (i.length == 0) { 
+			
+			i = this.contents() } 
+			
+			var s = i.top; 
+			var o = null; 
+			this.scroll(function () { 
+				
+				var r = i.offset().top - $(window).scrollTop();
+				
+				if (r > s) { 
+					if (n && o == "up") { 
+						s = r; return } o = "up"; 
+						e(r - s || 0) } 
+					else { 
+						if (n && o == "down") { 
+							s = r; return } o = "down"; 
+							t(s - r || 0) } s = r }) } 
+})(jQuery)
